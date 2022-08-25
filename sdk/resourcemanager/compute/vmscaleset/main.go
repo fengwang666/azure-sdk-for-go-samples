0f3acde024789d60e74f50a6521381bf1c747ff4
Feng Wang <feng.wang@databricks.com>
2022-08-25 09:15:25 -0700

    Measure vmss reimage latency

--- sdk/resourcemanager/compute/vmscaleset/main.go
+++ sdk/resourcemanager/compute/vmscaleset/main.go
@@ -5,23 +5,22 @@ package main
 
 import (
 	"context"
+	"log"
+	"os"
+	"time"
+
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore"
 	"github.com/Azure/azure-sdk-for-go/sdk/azcore/to"
 	"github.com/Azure/azure-sdk-for-go/sdk/azidentity"
 	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/network/armnetwork"
-	"github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/resources/armresources"
-	"log"
-	"os"
 )
 
 var (
 	subscriptionID     string
-	location           = "eastus"
-	resourceGroupName  = "sample-resource-group"
-	virtualNetworkName = "sample-virtual-network"
-	subnetName         = "sample-subnet"
-	vmScaleSetName     = "sample-vm-scale-set"
+	location           = "westus2"
+	resourceGroupName  = "fengwang-dev"
+	vmScaleSetName     = "reimage-test"
+	instanceId         = "0"
 )
 
 func main() {
@@ -35,205 +34,33 @@ func main() {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
-
-	resourceGroup, err := createResourceGroup(ctx, cred)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("resources group:", *resourceGroup.ID)
-
-	virtualNetwork, err := createVirtualNetwork(ctx, cred)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("virtual network:", *virtualNetwork.ID)
-
-	subnet, err := createSubnet(ctx, cred)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("subnet:", *subnet.ID)
-
-	vmss, err := createVMSS(ctx, cred, *subnet.ID)
-	if err != nil {
-		log.Fatal(err)
-	}
-	log.Println("virtual machine scale sets:", *vmss.ID)
-
-	keepResource := os.Getenv("KEEP_RESOURCE")
-	if len(keepResource) == 0 {
-		err := cleanup(ctx, cred)
+	var totalLatency int64
+	for i := 0; i < 100; i++ {
+		log.Printf("Run #%d", i)
+		start := time.Now()
+		err = reimageVM(ctx, cred)
 		if err != nil {
+			log.Printf("The average latency of reimage is %d ms", totalLatency/int64(i))
 			log.Fatal(err)
 		}
-		log.Println("cleaned up successfully.")
+		elapsed := time.Since(start).Milliseconds()
+		log.Printf("Spent %d ms", elapsed)
+		totalLatency += elapsed
 	}
+	log.Printf("The average latency of reimage is %d ms", totalLatency/100)
 }
 
-func createVirtualNetwork(ctx context.Context, cred azcore.TokenCredential) (*armnetwork.VirtualNetwork, error) {
-	virtualNetworkClient, err := armnetwork.NewVirtualNetworksClient(subscriptionID, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	pollerResp, err := virtualNetworkClient.BeginCreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		virtualNetworkName,
-		armnetwork.VirtualNetwork{
-			Location: to.Ptr(location),
-			Properties: &armnetwork.VirtualNetworkPropertiesFormat{
-				AddressSpace: &armnetwork.AddressSpace{
-					AddressPrefixes: []*string{
-						to.Ptr("10.1.0.0/16"),
-					},
-				},
-			},
-		},
-		nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := pollerResp.PollUntilDone(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-	return &resp.VirtualNetwork, nil
-}
-
-func createSubnet(ctx context.Context, cred azcore.TokenCredential) (*armnetwork.Subnet, error) {
-	subnetsClient, err := armnetwork.NewSubnetsClient(subscriptionID, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	pollerResp, err := subnetsClient.BeginCreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		virtualNetworkName,
-		subnetName,
-		armnetwork.Subnet{
-			Properties: &armnetwork.SubnetPropertiesFormat{
-				AddressPrefix: to.Ptr("10.1.0.0/24"),
-			},
-		},
-		nil)
-
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := pollerResp.PollUntilDone(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-	return &resp.Subnet, nil
-}
-
-func createVMSS(ctx context.Context, cred azcore.TokenCredential, subnetID string) (*armcompute.VirtualMachineScaleSet, error) {
+func reimageVM(ctx context.Context, cred azcore.TokenCredential) error {
 	vmssClient, err := armcompute.NewVirtualMachineScaleSetsClient(subscriptionID, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	pollerResp, err := vmssClient.BeginCreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		vmScaleSetName,
-		armcompute.VirtualMachineScaleSet{
-			Location: to.Ptr(location),
-			SKU: &armcompute.SKU{
-				Name:     to.Ptr("Basic_A0"), //armcompute.VirtualMachineSizeTypesBasicA0
-				Capacity: to.Ptr[int64](1),
-			},
-			Properties: &armcompute.VirtualMachineScaleSetProperties{
-				Overprovision: to.Ptr(false),
-				UpgradePolicy: &armcompute.UpgradePolicy{
-					Mode: to.Ptr(armcompute.UpgradeModeManual),
-					AutomaticOSUpgradePolicy: &armcompute.AutomaticOSUpgradePolicy{
-						EnableAutomaticOSUpgrade: to.Ptr(false),
-						DisableAutomaticRollback: to.Ptr(false),
-					},
-				},
-				VirtualMachineProfile: &armcompute.VirtualMachineScaleSetVMProfile{
-					OSProfile: &armcompute.VirtualMachineScaleSetOSProfile{
-						ComputerNamePrefix: to.Ptr("vmss"),
-						AdminUsername:      to.Ptr("sample-user"),
-						AdminPassword:      to.Ptr("Password01!@#"),
-					},
-					StorageProfile: &armcompute.VirtualMachineScaleSetStorageProfile{
-						ImageReference: &armcompute.ImageReference{
-							Offer:     to.Ptr("WindowsServer"),
-							Publisher: to.Ptr("MicrosoftWindowsServer"),
-							SKU:       to.Ptr("2019-Datacenter"),
-							Version:   to.Ptr("latest"),
-						},
-					},
-					NetworkProfile: &armcompute.VirtualMachineScaleSetNetworkProfile{
-						NetworkInterfaceConfigurations: []*armcompute.VirtualMachineScaleSetNetworkConfiguration{
-							{
-								Name: to.Ptr(vmScaleSetName),
-								Properties: &armcompute.VirtualMachineScaleSetNetworkConfigurationProperties{
-									Primary:            to.Ptr(true),
-									EnableIPForwarding: to.Ptr(true),
-									IPConfigurations: []*armcompute.VirtualMachineScaleSetIPConfiguration{
-										{
-											Name: to.Ptr(vmScaleSetName),
-											Properties: &armcompute.VirtualMachineScaleSetIPConfigurationProperties{
-												Subnet: &armcompute.APIEntityReference{
-													ID: to.Ptr(subnetID),
-												},
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-		},
-		nil,
-	)
-	if err != nil {
-		return nil, err
-	}
-
-	resp, err := pollerResp.PollUntilDone(ctx, nil)
-	if err != nil {
-		return nil, err
-	}
-	return &resp.VirtualMachineScaleSet, nil
-}
-
-func createResourceGroup(ctx context.Context, cred azcore.TokenCredential) (*armresources.ResourceGroup, error) {
-	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	resourceGroupResp, err := resourceGroupClient.CreateOrUpdate(
-		ctx,
-		resourceGroupName,
-		armresources.ResourceGroup{
-			Location: to.Ptr(location),
-		},
-		nil)
-	if err != nil {
-		return nil, err
-	}
-	return &resourceGroupResp.ResourceGroup, nil
-}
-
-func cleanup(ctx context.Context, cred azcore.TokenCredential) error {
-	resourceGroupClient, err := armresources.NewResourceGroupsClient(subscriptionID, cred, nil)
 	if err != nil {
 		return err
 	}
-
-	pollerResp, err := resourceGroupClient.BeginDelete(ctx, resourceGroupName, nil)
+	pollerResp, err := vmssClient.BeginReimage(ctx, resourceGroupName, vmScaleSetName,
+		&armcompute.VirtualMachineScaleSetsClientBeginReimageOptions{VMScaleSetReimageInput: &armcompute.VirtualMachineScaleSetReimageParameters{
+			InstanceIDs: []*string{
+				to.Ptr(instanceId)},
+		},
+		})
 	if err != nil {
 		return err
 	}
@@ -242,5 +69,6 @@ func cleanup(ctx context.Context, cred azcore.TokenCredential) error {
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
